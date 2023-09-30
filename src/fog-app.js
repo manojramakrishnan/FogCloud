415a9cb01c7e7bde77b5f4908abfeb53c1fad463
akhil murali <akhilmurali662@gmail.com>
2023-09-30 16:26:26 +0530

    added assets

---
+++ src/fog-app.js
@@ -0,0 +1,18 @@
+import '@polymer/polymer/polymer-legacy.js';
+import '@polymer/app-route/app-route.js';
+import '@polymer/app-route/app-location.js';
+import '@polymer/app-layout/app-layout.js';
+import '@polymer/app-layout/app-drawer-layout/app-drawer-layout.js';
+import '@polymer/app-drawer/app-drawer.js';
+import '@polymer/app-header/app-header.js';
+import '@polymer/app-toolbar/app-toolbar.js';
+import '@polymer/app-header-layout/app-header-layout.js';
+import '@polymer/paper-material/paper-material.js';
+import '@polymer/paper-icon-button/paper-icon-button.js';
+import '@polymer/iron-icons/iron-icons.js';
+import '@polymer/iron-pages/iron-pages.js';
+import '@polymer/iron-selector/iron-selector.js';
+import '@polymer/iron-media-query/iron-media-query.js';
+import '@polymer/paper-spinner/paper-spinner.js';
+import '@polymer/paper-toast/paper-toast.js';
+import { IronResizableBehavior } from '@polymer/iron-resizable-behavior/iron-resizable-behavior.js''
